86125d3d3353266fee825d827c3c5dd8ea9fab26
3ezzdinePro <rapboy.yacine@gmail.com>
2025-10-27 21:03:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // <-- default import
+import App from "./App.jsx"; // default import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
